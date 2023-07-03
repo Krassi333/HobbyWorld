2302f78f66bb5f89521806a5e76fa9287c4d9cca
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-07-03 16:02:01 +0300

    Update app routing

---
+++ HobbyWorld/src/app/app-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { CatalogComponent } from './features/catalog/catalog.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      }
+    ]
+  },
+  {
+    path: 'features',
+    children: [
+      {
+        path: 'catalog',
+        component: CatalogComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
