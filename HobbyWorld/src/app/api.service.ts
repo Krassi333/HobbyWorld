d2d8b693c322b022224d524cef1b894732ac73df
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-07-08 23:31:07 +0300

    Update api service

--- HobbyWorld/src/app/api.service.ts
+++ HobbyWorld/src/app/api.service.ts
@@ -3,23 +3,25 @@ import {HttpClient} from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
 import { IPost } from './types/post';
+import { Database, onValue, ref } from '@angular/fire/database'; 
 
 
-const {apiUrl}=environment;
+//const {apiUrl}=environment.firebase.databaseURL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http:HttpClient) {  }
+  constructor(private http:HttpClient,private database:Database) {  }
 
   getAllPosts() {
-    return this.http.get<IPost[]>(`${apiUrl}/posts`);
+  
+    return this.http.get<any>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/Posts`);
   }
 
   getPostById(id:string){
-    return this.http.get<IPost>(`${apiUrl}/posts/details/${id}`);
+    return this.http.get<IPost>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/Posts/${id}`);
   }
    
  
