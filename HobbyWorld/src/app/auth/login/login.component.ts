803f13172423754203efbde33ba229ab2f153537
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-07-12 15:22:19 +0300

    Create login page css

--- HobbyWorld/src/app/auth/login/login.component.ts
+++ HobbyWorld/src/app/auth/login/login.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthServiceService } from '../auth-service.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { IUser } from 'src/app/types/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('loginForm') loginForm!: NgForm;
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void { }
+
+  constructor(private authService: AuthServiceService,
+    private router: Router,
+    private matSnackBar: MatSnackBar) { }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    const data=this.loginForm.value;
+    this.authService.login(data).subscribe(
+      (response) => {
+        console.log(response);
+        this.loginForm.reset();
+        this.router.navigate(['/']);
+      },
+      (err) => {
+        const errorMessage = "Login failed - " + err.error.error.message;
+
+        this.matSnackBar.open(errorMessage, "OK", {
+          verticalPosition: "top",
+          horizontalPosition: "center",
+          panelClass: 'bg-danger'
+        });
+
+      }
+    )
+
+  }
 
 }
