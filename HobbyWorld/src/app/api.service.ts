26a81aa8d65ade8ec0a960506b4e09b1c7f61253
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-08-16 23:10:45 +0300

    Update aoi service

--- HobbyWorld/src/app/api.service.ts
+++ HobbyWorld/src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+import { environment } from 'src/environments/environment';
 import { IPost } from './types/post';
 import { Database, onValue, ref } from '@angular/fire/database'; 
 
@@ -16,11 +17,11 @@ export class ApiService {
 
   getAllPosts() {
   
-    return this.http.get<any>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/Posts`);
+    return this.http.get<any>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/posts`);
   }
 
   getPostById(id:string){
-    return this.http.get<IPost>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/Posts/${id}`);
+    return this.http.get<IPost>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/posts/${id}`);
   }
    
  
