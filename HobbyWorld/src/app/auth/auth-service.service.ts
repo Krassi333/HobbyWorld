45b652291438608cc8d6b29ed4b2b7a0143ef4ad
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-08-16 23:11:05 +0300

    Update auth service

--- HobbyWorld/src/app/auth/auth-service.service.ts
+++ HobbyWorld/src/app/auth/auth-service.service.ts
@@ -8,26 +8,48 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
+export class AuthService {
+  user: IUser | undefined;
 
+  get isLogged(): boolean {
+    return !!this.user;
+  }
 
-  constructor(private http: HttpClient, private database: Database) { }
+  constructor(private http: HttpClient, private database: Database) {
+    try {
+      const lsUser = localStorage.getItem('user') || '';
+      this.user = JSON.parse(lsUser);
+    }
+    catch {
+      this.user = undefined;
+    }
+  }
 
   register(data: IUser) {
     const apiKey = environment.firebase.apiKey;
-console.log({ ...data, returnSecureToken: true });
+    console.log(apiKey);
 
-    return this.http.post<IUser>(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
-      { ...data, returnSecureToken: true });
+    return this.http.post<IUser>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/User.json`,
+       data );
 
   }
 
   login(data: any) {
     const apiKey = environment.firebase.apiKey;
-    console.log(data);
-
-    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+    const findUser = this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
       { ...data, returnSecureToken: true });
 
+    localStorage.setItem('user', JSON.stringify(findUser));
+    return findUser;
+
+  }
+
+  getUsers(){
+    return this.http.get<IUser>(`https://hobbyworld-93522-default-rtdb.europe-west1.firebasedatabase.app/User.json` ); 
+  }
+
+  logout(): void {
+    this.user = undefined;
+    localStorage.removeItem('user');
   }
 }
